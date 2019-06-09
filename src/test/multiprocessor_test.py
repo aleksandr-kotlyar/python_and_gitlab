b754ffa6463539a2ba6377b23417d180cb982b47
aleksandr-kotlyar <ask.kotlyar@gmail.com>
2019-06-09 15:06:47 +0300

    Add allure-steps instead of docstrings * + changed `pprint` to `logging.info`

--- src/test/multiprocessor_test.py
+++ src/test/multiprocessor_test.py
@@ -1,6 +1,7 @@
+import logging
 from multiprocessing.dummy import Pool
-from pprint import pprint
 
+import allure
 import requests
 from assertpy import assert_that
 from assertpy import soft_assertions
@@ -12,28 +13,17 @@ def test_multiprocessor():
     sitemap_link: str = "https://bonus.qiwi.com/sitemap"
     link_startswith: str = "https://bonus.qiwi.com"
 
-    '''
-    # arrange
-    # Get sitemap page source
-    '''
-    page_source: requests.Response.text = requests.get(sitemap_link).text
+    with allure.step('arrange: Get sitemap page source'):
+        page_source: requests.Response.text = requests.get(sitemap_link).text
 
-    '''
-    # act
-    # Collect all links from sitemap
-    '''
-    list_sitemap_links: list = links_starting_with(page_source, link_startswith)
+    with allure.step('act: Collect all links from sitemap'):
+        list_sitemap_links: list = links_starting_with(page_source, link_startswith)
 
-    '''
-    # assertion
-    # Check all links return code 200
-    '''
-    # parallel checking in streams
-    threads: int = 15
-    with Pool(processes=threads) as pool:
-        # use soft assertions
-        with soft_assertions():
-            pool.map(assert_status_code_is_200, list_sitemap_links)
+    with allure.step('assert: Check all links return code 200'):
+        threads: int = 15
+        with Pool(processes=threads) as pool:
+            with soft_assertions():
+                pool.map(assert_status_code_is_200, list_sitemap_links)
 
 
 # test goes about 9 minutes
@@ -41,28 +31,17 @@ def test_one_thread():
     sitemap_link = "https://bonus.qiwi.com/sitemap"
     link_startswith = "https://bonus.qiwi.com"
 
-    '''
-    # arrange
-    # Get sitemap page source"
-    '''
-    page_source = requests.get(sitemap_link).text
+    with allure.step('arrange: Get sitemap page source'):
+        page_source = requests.get(sitemap_link).text
 
-    '''
-    # act
-    # Collect all links from sitemap
-    '''
-    list_sitemap_links = links_starting_with(page_source, link_startswith)
+    with allure.step('act: Collect all links from sitemap'):
+        list_sitemap_links = links_starting_with(page_source, link_startswith)
 
-    '''
-    # assertion
-    # Check all the links return code 200
-    '''
-    # one thread code
-    # use soft assertions
-    with soft_assertions():
-        # go through the whole links_list
-        for link in list_sitemap_links:
-            assert_status_code_is_200(link)
+    with allure.step('assert: Check all links return code 200'):
+        with soft_assertions():
+            # go through the whole links_list
+            for link in list_sitemap_links:
+                assert_status_code_is_200(link)
 
 
 def links_starting_with(page_source, schema):
@@ -74,14 +53,11 @@ def links_starting_with(page_source, schema):
         links_list.append(link.get('href'))
     # filter all links with by starts with schema
     filtered_links_list = list(filter(lambda lnk: lnk.startswith(schema), links_list))
-    pprint(filtered_links_list)
     # return filtered links list
     return filtered_links_list
 
 
 def assert_status_code_is_200(link):
-    # get status code
     code = requests.get(link).status_code
-    # assert status code is equal 200
-    print(link, code)
+    logging.info(link, code)
     assert_that(code, 'status code for link "' + link + '"').is_equal_to(200)
