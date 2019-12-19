7c75ea1d96ebc350d6a392d7f745cd21a5467b60
aleksandr-kotlyar <ask.kotlyar@gmail.com>
2019-12-20 00:17:40 +0300

    Example of fixture multiplication: 'test_parametrize_fixture_multiplication'

---
+++ src/test/test_fixtures.py
@@ -0,0 +1,29 @@
+import pytest
+
+
+@pytest.fixture(scope='function', params=['a', 'b', 'c'])
+def abc(request):
+    """ return one of abc: [a,b,c] """
+    return request.param
+
+
+@pytest.fixture(scope='function', params=[1, 2, 3])
+def onetwothree(request):
+    """ return one of onetwothree: [1,2,3] """
+    return request.param
+
+
+def test_parametrize_fixture_multiplication(abc, onetwothree):
+    """ Example of fixture multiplication.
+        For each of 'abc' to each of 'onetwothree':
+        a-1
+        a-2
+        a-3
+        b-1
+        b-2
+        b-3
+        c-1
+        c-2
+        c-3
+    """
+    assert abc == onetwothree
