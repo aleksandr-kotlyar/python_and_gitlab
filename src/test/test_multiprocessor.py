f24e4d26f235850b4dad0b2d7645f78b1a613d6b
aleksandr-kotlyar <ask.kotlyar@gmail.com>
2020-02-06 22:32:50 +0300

    list comprehension + small beautify imports and param-names

--- src/test/test_multiprocessor.py
+++ src/test/test_multiprocessor.py
@@ -1,4 +1,4 @@
-from multiprocessing.dummy import Pool
+from multiprocessing import dummy
 
 import pytest
 import requests
@@ -10,7 +10,7 @@ from src.main.allure_helpers import *
 
 
 # test goes about 45 seconds
-def test_multiprocessor():
+def test_multiprocessor_sitemap_checker():
     sitemap_link: str = "https://bonus.qiwi.com/sitemap"
     link_startswith: str = "https://bonus.qiwi.com"
 
@@ -22,19 +22,19 @@ def test_multiprocessor():
 
     with assertion(f'Check all links return code 200, Links count "{len(list_sitemap_links)}"'):
         threads = 15
-        with Pool(processes=threads) as pool:
+        with dummy.Pool(processes=threads) as pool:
             with soft_assertions():
                 pool.map(assert_status_code_is_200, list_sitemap_links)
 
 
 # test goes about 9 minutes
 @pytest.mark.skip(reason='gitlab execution time economy')
-def test_one_thread():
+def test_one_thread_sitemap_checker():
     sitemap_link = "https://bonus.qiwi.com/sitemap"
     link_startswith = "https://bonus.qiwi.com"
 
     with arrange('Get sitemap page source'):
-        page_source = requests.get(sitemap_link).text
+        page_source = requests.get(url=sitemap_link).text
 
     with act('Collect all links from sitemap'):
         list_sitemap_links = links_starting_with(page_source, link_startswith)
@@ -50,9 +50,7 @@ def links_starting_with(page_source, schema):
     # load page source in parse able way
     soup = BeautifulSoup(page_source, 'html.parser')
     # collect all links from "href" attribute on the page into links_list
-    links_list = []
-    for link in soup.find_all('a'):
-        links_list.append(link.get('href'))
+    links_list = [link.get('href') for link in soup.find_all('a')]
     # filter all links with by starts with schema
     filtered_links_list = list(filter(lambda lnk: lnk.startswith(schema), links_list))
     # return filtered links list
@@ -60,6 +58,6 @@ def links_starting_with(page_source, schema):
 
 
 def assert_status_code_is_200(link):
-    code = requests.get(link).status_code
+    code = requests.get(url=link).status_code
     logging.info(f'{code} {link}')
-    assert_that(code, 'status code for link "' + link + '"').is_equal_to(200)
+    assert_that(val=code, description='status code for link "' + link + '"').is_equal_to(200)
