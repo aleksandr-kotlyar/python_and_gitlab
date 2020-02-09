6b2a666874a6630febf47a4314aa6e039162815e
aleksandr-kotlyar <ask.kotlyar@gmail.com>
2020-02-09 20:15:45 +0300

    method 'wait_until_browser_title_equals' now takes 'selene.config' values as default params

--- src/main/new_window_waiter.py
+++ src/main/new_window_waiter.py
@@ -1,9 +1,9 @@
 import time
 
-from selene import browser
+from selene import browser, config
 
 
-def wait_until_browser_title_equals(title, timeout, period=0.05):
+def wait_until_browser_title_equals(title, timeout=config.timeout, poll_during_waits=config.poll_during_waits):
     """ Wait for browser window title name and raise Timeout exception if not found in time """
     must_end = time.time() + timeout
     while time.time() < must_end:
@@ -11,5 +11,5 @@ def wait_until_browser_title_equals(title, timeout, period=0.05):
             browser.driver().switch_to.window(handle)
             if browser.title() == title:
                 return True
-        time.sleep(period)
+        time.sleep(poll_during_waits)
     raise TimeoutError(f'Timeout {timeout} exceeded. Browser title not found: "{title}"')
