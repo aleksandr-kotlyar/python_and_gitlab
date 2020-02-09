bba168e77c4710e338df8b491c185992e15c47b4
aleksandr-kotlyar <ask.kotlyar@gmail.com>
2020-02-09 20:22:36 +0300

    update method description for 'wait_until_browser_title_equals'

--- src/main/new_window_waiter.py
+++ src/main/new_window_waiter.py
@@ -4,7 +4,7 @@ from selene import browser, config
 
 
 def wait_until_browser_title_equals(title, timeout=config.timeout, poll_during_waits=config.poll_during_waits):
-    """ Wait for browser window title name and raise Timeout exception if not found in time """
+    """ Wait for browser window title name to switch to and raise Timeout exception if not found in time """
     must_end = time.time() + timeout
     while time.time() < must_end:
         for handle in browser.driver().window_handles:
