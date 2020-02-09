78db2af8be2a16397399a5142b6afae99a5ae98b
aleksandr-kotlyar <ask.kotlyar@gmail.com>
2020-02-09 18:59:40 +0300

    method 'wait_until_browser_title_equals' now raises exception if timeout exceeded.

--- src/main/new_window_waiter.py
+++ src/main/new_window_waiter.py
@@ -1,16 +1,15 @@
 import time
 
 from selene import browser
-from selene.browser import driver
 
 
 def wait_until_browser_title_equals(title, timeout, period=0.05):
-    """ Wait for browser window title name """
+    """ Wait for browser window title name and raise Timeout exception if not found in time """
     must_end = time.time() + timeout
     while time.time() < must_end:
-        for handle in driver().window_handles:
-            driver().switch_to.window(handle)
+        for handle in browser.driver().window_handles:
+            browser.driver().switch_to.window(handle)
             if browser.title() == title:
                 return True
         time.sleep(period)
-    return False
+    raise TimeoutError(f'Timeout {timeout} exceeded. Browser title not found: "{title}"')
