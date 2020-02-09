7772df25812c9dd2b07c4117ada29c9d4525b5a9
aleksandr-kotlyar <ask.kotlyar@gmail.com>
2020-02-10 01:03:54 +0300

    remove poll_during_waits from params of 'wait_until_browser_title_equals' method

--- src/main/new_window_waiter.py
+++ src/main/new_window_waiter.py
@@ -4,15 +4,16 @@ from selene.core import match
 from selene.support.shared import browser, config
 
 
-def wait_until_browser_title_equals(title, timeout=config.timeout, poll_during_waits=config.poll_during_waits):
-    """ Wait for browser window title name to switch to and raise Timeout exception if not found in time """
+def wait_until_browser_title_equals(title, timeout=config.timeout):
+    """ Wait for browser window title name to switch to and raise Timeout exception
+    if not found in time """
     must_end = time.time() + timeout
     while time.time() < must_end:
         for handle in browser.driver().window_handles:
             browser.driver().switch_to.window(handle)
             if browser.title() == title:
                 return
-        time.sleep(poll_during_waits)
+        time.sleep(config.poll_during_waits)
     raise TimeoutError(f'Timeout {timeout} exceeded. Browser title not found: "{title}"')
 
 
