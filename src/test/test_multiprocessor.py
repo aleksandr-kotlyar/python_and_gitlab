393ae98bee1997dc989560bb6b62b4360abb4a68
aleksandr-kotlyar <ask.kotlyar@gmail.com>
2020-02-16 08:19:55 +0300

    16 feb 2020: extract common fixture 'sitemap_urls()' and rename vars a bit

--- src/test/test_multiprocessor.py
+++ src/test/test_multiprocessor.py
@@ -10,56 +10,57 @@ from bs4 import BeautifulSoup
 from src.main.allure_helpers import arrange, act, assertion
 
 
-# test goes about 45 seconds
-def test_multiprocessor_sitemap_checker():
-    """ Sitemap checking for 200 status code (multi thread) """
-    sitemap_link: str = "https://bonus.qiwi.com/sitemap"
-    link_startswith: str = "https://bonus.qiwi.com"
+@pytest.fixture(scope='function')
+def sitemap_urls():
+    """ Request sitemap html to get all links startswith """
 
     with arrange('Get sitemap page source'):
-        page_source = requests.get(sitemap_link).text
+        sitemap_url = "https://bonus.qiwi.com/sitemap"
+        startswith = "https://bonus.qiwi.com"
+        sitemap_pagesource = requests.get(sitemap_url).text
 
     with act('Collect all links from sitemap'):
-        list_sitemap_links = links_starting_with(page_source, link_startswith)
+        sitemap_urls = find_urls_on_sitemap(sitemap_pagesource, startswith)
 
-    with assertion(f'Check all links return code 200, Links count "{len(list_sitemap_links)}"'):
+    return sitemap_urls
+
+
+# test goes about 45 seconds
+def test_multiprocessor_sitemap_checker(sitemap_urls):
+    """ Sitemap checking for 200 status code (multi thread) """
+
+    with assertion('Check all links return code 200'):
         threads = 15
         with dummy.Pool(processes=threads) as pool:
             with soft_assertions():
-                pool.map(assert_status_code_is_200, list_sitemap_links)
+                pool.map(assert_status_code, sitemap_urls)
 
 
 # test goes about 9 minutes
 @pytest.mark.skip(reason='gitlab execution time economy')
-def test_one_thread_sitemap_checker():
+def test_one_thread_sitemap_checker(sitemap_urls):
     """ Sitemap checking for 200 status code (one thread) """
-    sitemap_link = "https://bonus.qiwi.com/sitemap"
-    link_startswith = "https://bonus.qiwi.com"
-
-    with arrange('Get sitemap page source'):
-        page_source = requests.get(url=sitemap_link).text
-
-    with act('Collect all links from sitemap'):
-        list_sitemap_links = links_starting_with(page_source, link_startswith)
 
     with assertion('Check all links return code 200'):
-        for link in list_sitemap_links:
+        for url in sitemap_urls:
             with soft_assertions():
-                assert_status_code_is_200(link)
+                assert_status_code(url)
 
 
-def links_starting_with(page_source, schema):
+def find_urls_on_sitemap(pagesource, startswith):
+    """ Parse sitemap html to get all links startswith """
     # load page source in parse able way
-    soup = BeautifulSoup(page_source, 'html.parser')
+    soup = BeautifulSoup(pagesource, 'html.parser')
     # collect all links from "href" attribute on the page into links_list
-    links_list = [link.get('href') for link in soup.find_all('a')]
+    urls_list = [link.get('href') for link in soup.find_all('a')]
     # filter all links with by starts with schema
-    filtered_links_list = list(filter(lambda lnk: lnk.startswith(schema), links_list))
+    urls_filtered_list = list(filter(lambda lnk: lnk.startswith(startswith), urls_list))
     # return filtered links list
-    return filtered_links_list
+    return urls_filtered_list
 
 
-def assert_status_code_is_200(link):
-    code = requests.get(url=link).status_code
-    logging.info(f'{code} {link}')
-    assert_that(val=code, description='status code for link "' + link + '"').is_equal_to(200)
+def assert_status_code(url, status_code=200):
+    """ Request url to assert response status code """
+    code = requests.get(url=url).status_code
+    logging.info(f'{code} {url}')
+    assert_that(val=code, description='status code for link "' + url + '"').is_equal_to(status_code)
