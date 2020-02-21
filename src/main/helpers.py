5623e2fb38fbb079e8a0a60fab929f800caa39f5
aleksandr-kotlyar <ask.kotlyar@gmail.com>
2020-02-22 00:24:01 +0300

    21 feb 2020: few more changes 2. conftest.py: Add logger catching to allure-steps 3. allure_helpers.py: Move allure-logger-wrapper from helpers.py 4. pytest.ini: Add no capture log.txt to allure 5. test_schema_validators.py: Rename attach name from API to URI.

--- src/main/helpers.py
+++ src/main/helpers.py
@@ -1,11 +1,10 @@
 import json
-import logging
 import os
-from functools import wraps
 
 import allure
-import curlify
 from requests import Session, Response
+
+from src.main.allure_helpers import add_allure_logger
 
 
 def read_json(filename):
@@ -21,49 +20,11 @@ def read_json(filename):
         return schema
 
 
-def add_allure_and_logger(function):
-    """
-    Allure/Logger decorator for logging information about request
-    """
-
-    @wraps(function)
-    def wrapper(*args, **kwargs):
-        response = function(*args, **kwargs)
-        msg = curlify.to_curl(response.request)
-        logging.info(f'{response.status_code} {msg}')
-        allure.attach(
-            body=msg.encode('utf8'),
-            name=f'Request {response.status_code} {response.request.method} {response.request.url}',
-            attachment_type=allure.attachment_type.TEXT,
-            extension='txt')
-
-        try:
-            response.json()
-            allure.attach(
-                body=json.dumps(response.json(), indent=4, ensure_ascii=False).encode('utf8'),
-                name=f'Response REST API {response.status_code} {response.request.method} {response.request.url}',
-                attachment_type=allure.attachment_type.JSON,
-                extension='json')
-
-        except ValueError as error:
-            logging.error('RESPONSE IN NOT JSON FORMAT')
-            allure.attach(
-                body=response.text.encode('utf8'),
-                name=f'NOT JSON Response {response.status_code} {response.request.method} {response.request.url}',
-                attachment_type=allure.attachment_type.TEXT,
-                extension='txt')
-            raise error
-        return response
-
-    return wrapper
-
-
 class MySession(Session):
-
     def __init__(self):
         super().__init__()
 
-    @add_allure_and_logger
+    @add_allure_logger
     def request(self, method, url, **kwargs) -> Response:
         """ Log request/response to allure and info"""
 
