7aaeb7e4b0227001020e0ad6da363deadf2e889e
aleksandr-kotlyar <ask.kotlyar@gmail.com>
2020-10-14 00:17:05 +0300

    Download artifacts from gitlab latest succeeded 'pages' job.

---
+++ artifacts.py
@@ -0,0 +1,31 @@
+import os
+
+import requests
+
+CI_PROJECT_ID = os.environ.get('CI_PROJECT_ID')
+CI_COMMIT_REF_NAME = os.environ.get('CI_COMMIT_REF_NAME')
+PRIVATE_TOKEN = os.environ.get('PRIVATE_TOKEN')
+
+url = f'https://gitlab.com/api/v4/projects/{CI_PROJECT_ID}/jobs'
+
+payload = {
+    'ref': CI_COMMIT_REF_NAME,
+    'job': 'pages',
+    'status': 'success'
+}
+headers = {
+    'PRIVATE-TOKEN': PRIVATE_TOKEN,
+}
+
+response = requests.request("GET", url, headers=headers, params=payload)
+
+latest_job_id = response.json()[0]['id']
+
+url = f'https://gitlab.com/api/v4/projects/{CI_PROJECT_ID}/jobs/' \
+      f'{latest_job_id}/artifacts'
+
+response = requests.request("GET", url, headers=headers)
+
+artifacts = open('artifacts.zip', 'wb')
+artifacts.write(response.content)
+artifacts.close()
