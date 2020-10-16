5ae43bc01ffc9c5f1dbc3a4a2d425a8e8cd892c2
aleksandr-kotlyar <ask.kotlyar@gmail.com>
2020-10-16 11:20:12 +0300

    add fail exit code on failed comparison

--- quality_control.py
+++ quality_control.py
@@ -1,4 +1,3 @@
-import logging
 import os
 
 import requests
@@ -8,17 +7,17 @@ SOURCE_BRANCH = os.getenv('CI_COMMIT_REF_NAME')
 PRIVATE_TOKEN = os.getenv('PRIVATE_TOKEN')
 
 
-def get_opened_merge_request_of_source_branch():
+def get_opened_merge_requests_of_source_branch(project, branch):
     """Get last opened merge request for source branch"""
-    response = requests.request(method='get', url=f'https://gitlab.com/api/v4/projects/{CI_PROJECT_ID}/merge_requests',
-                                params={'source_branch': SOURCE_BRANCH, 'state': 'opened'})
+    response = requests.request(method='get', url=f'https://gitlab.com/api/v4/projects/{project}/merge_requests',
+                                params={'source_branch': branch, 'state': 'opened'})
     assert response.status_code == 200
-    return response.json()[0]
+    return response.json()
 
 
 def get_target_branch_of_merge_request(merge_request):
-    logging.info(merge_request['target_branch'])
-    return merge_request['target_branch']
+    print(merge_request[0]['target_branch'])
+    return merge_request[0]['target_branch']
 
 
 def get_latest_job_artifact_of_branch(branch):
@@ -31,9 +30,16 @@ def get_latest_job_artifact_of_branch(branch):
     return response.text
 
 
-mr = get_opened_merge_request_of_source_branch()
+merge_requests = get_opened_merge_requests_of_source_branch(CI_PROJECT_ID, SOURCE_BRANCH)
+
+if not merge_requests:
+    exit(0)
+
+mr = merge_requests[0]
 target_branch = get_target_branch_of_merge_request(mr)
 target_score = get_latest_job_artifact_of_branch(target_branch)
-source_score = get_latest_job_artifact_of_branch('linter')
+source_score = get_latest_job_artifact_of_branch(SOURCE_BRANCH)
 
-assert source_score < target_score
+if source_score < target_score:
+    print('Quality become lower:', source_score, '<', target_score)
+    exit(1)
