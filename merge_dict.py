3cd60d3111f9c03df5ea43d89ec5efb5debf458f
aleksandr-kotlyar <ask.kotlyar@gmail.com>
2020-11-10 02:40:30 +0300

    save gh_unique_clones.json to artifacts path and add link to file into badge link_url; add method which merge two lists of dicts into one by timestamp same value with higher unique value; add logs.

---
+++ merge_dict.py
@@ -0,0 +1,20 @@
+from pprint import pprint
+
+
+def merge_two_lists_of_dicts_by_key_condition(ld1, ld2):
+    """Update dict with same timestamp by higher 'unique' key value."""
+    listdict: list = ld1 + ld2
+    for i in range(len(listdict) - 1):
+        for j in range(i + 1, len(listdict)):
+            if listdict[i]['timestamp'] == listdict[j]['timestamp']:
+                if listdict[i]['uniques'] <= listdict[j]['uniques']:
+                    listdict.remove(listdict[i])
+                else:
+                    listdict.remove(listdict[j])
+                break
+
+    print(f'list1: {len(ld1)}')
+    print(f'list2: {len(ld2)}')
+    pprint(f'merged_list\n{listdict}')
+    print(f'merged_list: {len(listdict)}')
+    return listdict
--- stat_github.py
+++ stat_github.py
@@ -5,6 +5,8 @@ from pprint import pprint
 import anybadge
 import requests
 
+import merge_dict
+
 PRIVATE_TOKEN = os.environ.get('PRIVATE_TOKEN')
 TOKEN = os.environ.get('TOKEN')
 OWNER = os.environ.get('CI_PROJECT_NAMESPACE')
@@ -12,25 +14,28 @@ REPO = os.environ.get('CI_PROJECT_NAME')
 CI_PROJECT_ID = os.environ.get('CI_PROJECT_ID')
 GH_UNIQUE_CLONES_BADGE = os.environ.get('GH_UNIQUE_CLONES_BADGE')
 CI_JOB_URL = os.environ.get('CI_JOB_URL')
+LOG_FILE = 'gh_unique_clones.json'
 
 
 def get_current_uniques_stat():
+    print('get_current_uniques_stat')
     stat = requests.get(
         url='https://api.github.com/repos/{0}/{1}/traffic/clones'.format(OWNER, REPO),
         headers={'Authorization': f'token {TOKEN}'},
-        params={'per': 'week'})
+        params={'per': 'day'})
 
     if stat.status_code != 200:
         return 0
 
     pprint(stat.json())
-    return stat.json()['uniques']
+    return stat.json()
 
 
 def get_archive_uniques_stat():
+    print('get_archive_uniques_stat')
     stat = requests.get(
         url=f'https://gitlab.com/api/v4/projects/{CI_PROJECT_ID}'
-            f'/jobs/artifacts/master/raw/gh_unique_clones.log?job=stats:github:unique:clones')
+            f'/jobs/artifacts/master/raw/{LOG_FILE}?job=stats:github:unique:clones')
 
     if stat.status_code != 200:
         return 0
@@ -40,31 +45,37 @@ def get_archive_uniques_stat():
 
 
 def sum_uniques_stats(current, archive):
+    print('sum_uniques_stats')
     if archive == 0:
         return current
 
-    stats = current + archive
+    stats = merge_dict.merge_two_lists_of_dicts_by_key_condition(current, archive)
     pprint(stats)
     return stats
 
 
 def save_uniques_stats(stats):
-    with open('gh_unique_clones.log', 'w') as file:
+    print('save_uniques_stats')
+    with open(LOG_FILE, 'w') as file:
         file.write(str(stats))
 
 
 def public_uniques_stats(stats):
+    print('public_uniques_stats')
     badge = anybadge.Badge(label='downloads/unique',
                            value=stats,
                            default_color='green',
                            num_padding_chars=1)
     badge.write_badge('gh_unique_clones.svg', overwrite=True)
 
-    requests.put(
+    badge_put = requests.put(
         url=f'https://gitlab.com/api/v4/projects/{CI_PROJECT_ID}/badges/{GH_UNIQUE_CLONES_BADGE}',
-        json={'image_url': f'{CI_JOB_URL}/artifacts/raw/gh_unique_clones.svg'},
+        json={'link_url': f'{CI_JOB_URL}/artifacts/raw/{LOG_FILE}',
+              'image_url': f'{CI_JOB_URL}/artifacts/raw/gh_unique_clones.svg'},
         headers={'PRIVATE-TOKEN': PRIVATE_TOKEN})
-    pprint('badge published')
+
+    if badge_put.status_code in [200, 201]:
+        pprint('badge published')
 
 
 CURRENT = get_current_uniques_stat()
