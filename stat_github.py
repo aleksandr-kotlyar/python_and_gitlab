432dcb19df5b1049a5885d3a8b334a85c1b84c54
aleksandr-kotlyar <ask.kotlyar@gmail.com>
2020-11-13 02:23:53 +0300

    archive stat split text[1:-1] to remove ( ) chars

--- stat_github.py
+++ stat_github.py
@@ -38,7 +38,7 @@ def get_archive_uniques_stat():
     if stat.status_code != 200:
         return []
 
-    stat = stat.text
+    stat = stat.text[1:-1]
     pprint(stat)
 
     stat = json.loads(stat)
