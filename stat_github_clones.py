a2da285c82599cabd4beda80720612b4f6377dab
aleksandr-kotlyar <ask.kotlyar@gmail.com>
2020-11-13 04:19:48 +0300

    fix badgeid to GH_COUNT_CLONES_BADGE

--- stat_github_clones.py
+++ stat_github_clones.py
@@ -5,7 +5,7 @@ from pprint import pprint
 import merge_dict
 from stat_github import get_current_stat, get_archive_stat, save_stats, public_stats
 
-GH_UNIQUE_CLONES_BADGE = os.environ.get('GL_COUNT_CLONES_BADGE')
+GH_COUNT_CLONES_BADGE = os.environ.get('GH_COUNT_CLONES_BADGE')
 LOG_FILE = 'gh_clones.json'
 BADGE_SVG = 'gh_clones.svg'
 
@@ -23,4 +23,4 @@ ARCHIVE = get_archive_stat('stats:github:clones', LOG_FILE)
 MERGED = merge_dict.merge_two_lists_of_dicts_by_key_condition(CURRENT, ARCHIVE)
 SUMMARY: int = sum_clones_stats(MERGED)
 save_stats(MERGED, LOG_FILE)
-public_stats(SUMMARY, 'downloads/clones', BADGE_SVG, GH_UNIQUE_CLONES_BADGE, LOG_FILE)
+public_stats(SUMMARY, 'downloads/clones', BADGE_SVG, GH_COUNT_CLONES_BADGE, LOG_FILE)
