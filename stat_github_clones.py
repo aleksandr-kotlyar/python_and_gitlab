b5b212b2be9983b8447657cfdff092c948c764f1
aleksandr-kotlyar <ask.kotlyar@gmail.com>
2020-11-13 04:12:06 +0300

    add stat github clones job and module

---
+++ stat_github_clones.py
@@ -0,0 +1,26 @@
+# pylint: disable=missing-function-docstring
+import os
+from pprint import pprint
+
+import merge_dict
+from stat_github import get_current_stat, get_archive_stat, save_stats, public_stats
+
+GH_UNIQUE_CLONES_BADGE = os.environ.get('GL_COUNT_CLONES_BADGE')
+LOG_FILE = 'gh_clones.json'
+BADGE_SVG = 'gh_clones.svg'
+
+
+def sum_clones_stats(stats) -> int:
+    print('sum_clones_stats')
+
+    summary = sum(s['count'] for s in stats)
+    pprint(summary)
+    return summary
+
+
+CURRENT = get_current_stat()['clones']
+ARCHIVE = get_archive_stat('stats:github:clones', LOG_FILE)
+MERGED = merge_dict.merge_two_lists_of_dicts_by_key_condition(CURRENT, ARCHIVE)
+SUMMARY: int = sum_clones_stats(MERGED)
+save_stats(MERGED, LOG_FILE)
+public_stats(SUMMARY, 'downloads/clones', BADGE_SVG, GH_UNIQUE_CLONES_BADGE, LOG_FILE)
