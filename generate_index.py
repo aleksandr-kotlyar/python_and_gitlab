9e14f3080ae77cef56bb9cc795627f4b98dfed41
aleksandr-kotlyar <ask.kotlyar@gmail.com>
2021-03-10 02:38:00 +0300

    make generate_index.py taking arguments

--- generate_index.py
+++ generate_index.py
@@ -1,6 +1,7 @@
 import os
+import sys
 
-INDEX_TEXT_START = """<!DOCTYPE html>
+indexTextStart = """<!DOCTYPE html>
 <html>
 <head><title>Index of {folderPath}</title></head>
 <body>
@@ -11,7 +12,7 @@ INDEX_TEXT_START = """<!DOCTYPE html>
             <a href='../'>../</a>
         </li>
 """
-INDEX_TEXT_END = """
+indexTextEnd = """
     </ul>
 </body>
 </html>
@@ -19,28 +20,30 @@ INDEX_TEXT_END = """
 
 
 def index_folder(folder_path):
-    """Reindex folder from folder_path."""
     print("Indexing: " + folder_path + '/')
     # Getting the content of the folder
     files = os.listdir(folder_path)
     # If Root folder, correcting folder name
     root = folder_path
-    if folder_path == '.':
-        root = 'Root'
-    index_text = INDEX_TEXT_START.format(folderPath=root)
-    for file in files:
+    if folder_path.startswith('public'):
+        root = folder_path.replace('public', 'gitlab-allure-history')
+    index_text = indexTextStart.format(folderPath=root)
+    for file in sorted(files):
         # Avoiding index.html files
         if file != 'index.html':
             index_text += "\t\t<li>\n\t\t\t<a href='" + file + "'>" + file + "</a>\n\t\t</li>\n"
         # Recursive call to continue indexing
         # if os.path.isdir(folder_path + '/' + file):
         #     index_folder(folder_path + '/' + file)
-    index_text += INDEX_TEXT_END
+    index_text += indexTextEnd
     # Create or override previous index.html
     index = open(folder_path + '/index.html', "w")
     # Save indexed content to file
     index.write(index_text)
 
 
+folder_path = sys.argv[1]
+
+
 # Indexing root directory (Script position)
-index_folder('./public/stage')
+index_folder(folder_path)
