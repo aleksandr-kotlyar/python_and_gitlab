70f52a2d82ee10ee4f2484d6a3544bcd113868a5
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2026-04-29 21:23:25 +0300

    ci(qa): enforce Python 3.12 quality gates and remove network flakiness from API smoke

--- fixtures_session.py
+++ fixtures_session.py
@@ -1,7 +1,12 @@
 # pylint: disable=missing-function-docstring
+
 import pytest
 
-from src.main.session import ApiSession, HttpbinApiSessionLevelOne, HttpbinApiSessionLevelTwo
+from src.main.session import (
+    ApiSession,
+    HttpbinApiSessionLevelOne,
+    HttpbinApiSessionLevelTwo,
+)
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -23,6 +28,51 @@ def httpbin3() -> HttpbinApiSessionLevelTwo:
 
 
 @pytest.fixture(scope='session')
-def httpbin() -> HttpbinApiSessionLevelTwo:
-    with HttpbinApiSessionLevelTwo() as session:
-        yield session
+def httpbin():
+    class _FakeResponse:
+        def __init__(self, payload: dict):
+            self._payload = payload
+
+        def json(self):
+            return self._payload
+
+    class _FakeHttpbin:
+        @staticmethod
+        def request(method, url):
+            if method == 'GET' and url == '/get':
+                return _FakeResponse(
+                    {
+                        'args': {},
+                        'headers': {
+                            'Accept': '*/*',
+                            'Accept-Encoding': 'gzip, deflate',
+                            'Host': 'httpbin.org',
+                            'User-Agent': 'python-requests/fake',
+                        },
+                        'origin': '127.0.0.1',
+                        'url': 'https://httpbin.org/get',
+                    }
+                )
+            if method == 'PATCH' and url == '/patch':
+                body = ''
+                return _FakeResponse(
+                    {
+                        'args': {},
+                        'data': body,
+                        'files': {},
+                        'form': {},
+                        'headers': {
+                            'Accept': '*/*',
+                            'Accept-Encoding': 'gzip, deflate',
+                            'Content-Length': str(len(body)),
+                            'Host': 'httpbin.org',
+                            'User-Agent': 'python-requests/fake',
+                        },
+                        'json': None,
+                        'origin': '127.0.0.1',
+                        'url': 'https://httpbin.org/patch',
+                    }
+                )
+            raise ValueError(f'Unsupported fake httpbin request: method={method}, url={url}')
+
+    return _FakeHttpbin()
