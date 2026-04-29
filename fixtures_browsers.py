7b73ad2a09ed7e6e38ca55ea749aeb2769f84fc0
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2026-04-29 21:11:27 +0300

    build: add black 25.1.0 and adopt targeted readability formatting

--- fixtures_browsers.py
+++ fixtures_browsers.py
@@ -38,28 +38,30 @@ def choose_driver(is_remote, t_browser):
 
 
 def custom_driver(t_browser):
-    """ Custom driver """
+    """Custom driver"""
     logging.debug('custom driver config start')
     if t_browser == 'chrome':
         driver = webdriver.Chrome(
             service=ChromeService(ChromeDriverManager().install()),
-            options=headless_chrome_options()
+            options=headless_chrome_options(),
         )
     else:
         raise ValueError('t_browser does not set')
     driver.set_page_load_timeout(10)
-    browser = Browser(Config(
-        driver=driver,
-        timeout=10,
-        window_width=1366,
-        window_height=1200,
-    ))
+    browser = Browser(
+        Config(
+            driver=driver,
+            timeout=10,
+            window_width=1366,
+            window_height=1200,
+        )
+    )
     logging.debug('custom driver config finish')
     return browser
 
 
 def headless_chrome_options():
-    """ Custom chrome options """
+    """Custom chrome options"""
     logging.info('set chromedriver options start')
     chrome_options = Options()
     chrome_options.set_capability("pageLoadStrategy", "eager")
@@ -77,30 +79,34 @@ def headless_chrome_options():
 
 
 def remote_driver(t_browser, page_load_strategy=None):
-    """ Remote driver """
+    """Remote driver"""
     logging.debug('remote driver config start')
     remote_mapping = {
         'chrome': {
             'command_executor': 'http://selenium__standalone-chrome:4444/wd/hub',
-            'options': webdriver.ChromeOptions()
+            'options': webdriver.ChromeOptions(),
         },
         'firefox': {
             'command_executor': 'http://selenium__standalone-firefox:4444/wd/hub',
-            'options': webdriver.FirefoxOptions()
-        }
+            'options': webdriver.FirefoxOptions(),
+        },
     }
     if page_load_strategy:
         desired_capabilities = webdriver.DesiredCapabilities().CHROME
         desired_capabilities["pageLoadStrategy"] = "eager"
 
-    driver = webdriver.Remote(command_executor=remote_mapping[t_browser]['command_executor'],
-                              options=remote_mapping[t_browser]['options'])
+    driver = webdriver.Remote(
+        command_executor=remote_mapping[t_browser]['command_executor'],
+        options=remote_mapping[t_browser]['options'],
+    )
     driver.set_page_load_timeout(20)
-    browser = Browser(Config(
-        driver=driver,
-        timeout=10,
-        window_width=1500,
-        window_height=1200,
-    ))
+    browser = Browser(
+        Config(
+            driver=driver,
+            timeout=10,
+            window_width=1500,
+            window_height=1200,
+        )
+    )
     logging.debug('remote driver config finish')
     return browser
--- src/test/test_api.py
+++ src/test/test_api.py
@@ -4,10 +4,13 @@ from pytest import mark
 from src.test.api_helpers import validate_json_schema
 
 
-@mark.parametrize('method, path, schema', [
-    ('GET', '/get', 'get.json'),
-    ('PATCH', '/patch', 'patch.json'),
-])
+@mark.parametrize(
+    'method, path, schema',
+    [
+        ('GET', '/get', 'get.json'),
+        ('PATCH', '/patch', 'patch.json'),
+    ],
+)
 @allure.title('HTTPBIN {method} "{path}" matches schema "{schema}"')
 def test_httpbin_contract(method, path, schema, httpbin):
     response = httpbin.request(method=method, url=path)
